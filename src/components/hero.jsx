0927565c7d836205da62d767feb5482c8a45fbcd
Raj kumar <186265760+Rajkr76@users.noreply.github.com>
2025-07-24 23:37:13 +0530

    fixed layout issue for smaller devices (left side scrolling)

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -11,8 +11,8 @@ function Hero() {
          <h1 className='font-sans font-medium text-base'>	&#9679; Menu</h1>
       </nav>
       <section className='flex justify-center px-2 sm:px-0'>
-        <div className='relative '>
-          <img src="https://images.unsplash.com/photo-1501685532562-aa6846b14a0e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="home image" className='hidden sm:block h-[90vh] w-[96vw] px-2 py-2 object-cover object-center ' />
+        <div className='relative hidden sm:block '>
+          <img src="https://images.unsplash.com/photo-1501685532562-aa6846b14a0e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="home image" className=' h-[90vh] w-[96vw] px-2 py-2 object-cover object-center ' />
           <FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff"}} size="2xl" className='z-10 absolute top-[36rem] left-[1400px] sm:h-[70vh] md:h-[80vh] lg:h-[90vh]'/>
           <FontAwesomeIcon icon={faWindowMinimize} style={{color: "#ffffff", transform: "rotate(90deg)"}} size="2xl" className='z-10 absolute top-[36rem] left-[1380px] '/>
            <FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff", transform: "rotate(180deg)"}} size="2xl" className='z-10 absolute top-[36rem] left-[1340px]'/>
